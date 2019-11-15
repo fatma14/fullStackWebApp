4bf6a19ec1fa3da9814c8f648089cbcaebb24612
fatma14 <fatmafadhli1@gmail.com>
2019-11-15 19:50:51 +0100

    signuppage

--- fullStackWeb/webApp/routes/index.js
+++ fullStackWeb/webApp/routes/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const router = express.Router();
-
+const User = require("../models/User");
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.post("/signup", (req, res, next) => {
-  res.redirect("signup.hbs");
+router.post("/content", (req, res, next) => {
+  User.create({
+    username,
+    password,
+    birthday,
+    email
+  })
+    .then(newUser => {
+      res.redirect("content.hbs");
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
