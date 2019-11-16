541586d73696e22478d5c73a516715061d38a400
NinoGlonti <54588901+NinoGlonti@users.noreply.github.com>
2019-11-16 16:43:53 +0100

    password hash

--- fullStackWeb/webApp/routes/index.js
+++ fullStackWeb/webApp/routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 /* GET home page */
@@ -7,45 +8,50 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.post("/content", (req, res, next) => {
-  const {username, password, birthday, email } = req.body
-  User.create({
-    username,
-    password,
-    email,
-    birthday
-  })
-    .then(newUser => {
-      res.render("content");
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
+
+
 
 router.post("/content", (req, res, next) => {
- const {username, password, birthday, email } = req.body
-/*User.create({
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email,
-    birthday: req.body.birthday
-  })*/
-
-  if(!username) {
+ const {username, password, birthday, email } = req.body;
+  
+ if(!username) {
     res.render ("index.hbs" , {message: "You have to fill the field"});
     return
   }  if(password.length < 4) {
     res.render("index.hbs", {message: "Password is too short"})
   }
-})
-    /*User.findOne({username:username}) 
-    .then(
-      if(found =>
-      res.render("index.hbs", {message: "User already exists"})
-      )
+
+  
+  User.findOne({username})
+  .then(found =>{
+    if(found) {
+    res.render("index.hbs", {message: "User already exists"})
+    return;
+    }
+
+     bcrypt.genSalt()
+     .then(salt =>{
+        return bcrypt.hash(password, salt)
+    })
+    .then(hash => {
+      return User.create({username: username, password: hash, email: email, birthday: birthday})
+    })
       })
-      */
+    })
+    
+      
+
+    
+/*
+User.create({
+  username,
+  password,
+  email: email,
+  birthday: birthday
+})
+*/
+
+  
     
    /*.then(newUser => {
       res.render("content");
