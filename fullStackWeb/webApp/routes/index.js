53e1f659c535039d8e8ef0e5d6db89f463aeed81
fatma14 <fatmafadhli1@gmail.com>
2019-11-16 15:24:37 +0100

    fix merge conflict

--- fullStackWeb/webApp/routes/index.js
+++ fullStackWeb/webApp/routes/index.js
@@ -5,29 +5,22 @@ const User = require("../models/User");
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
-<<<<<<< HEAD
 });
 
 router.post("/content", (req, res, next) => {
   const {username, password, birthday, email } = req.body
   User.create({
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email,
-    birthday: req.body.birthday
+    username,
+    password,
+    email,
+    birthday
   })
     .then(newUser => {
-<<<<<<< HEAD
-      res.render("content.hbs");
-=======
       res.render("content");
->>>>>>> e9f9a0898d9df817e452c82adc45d1c1635cdb4f
     })
     .catch(err => {
       console.log(err);
     });
-=======
->>>>>>> master
 });
 
 router.post("/content", (req, res, next) => {
